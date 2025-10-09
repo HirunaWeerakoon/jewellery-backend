7bae8ed588da170a9be05bc7e5c1856ef28b352c
HirunaWeerakoon <it24610788@my.sliit.lk>
2025-10-09 13:37:58 +0530

    Remove the comment '// getters and setters' from Product.java

---
+++ src/main/java/com/example/jewellery_backend/entity/Product.java
@@ -0,0 +1,23 @@
+// Product class with other code
+
+public class Product {
+    private String name;
+    private double price;
+
+    // getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
