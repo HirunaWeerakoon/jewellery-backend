1fdf5c45c1692f787e0126c7c2605904ff460ea7
HirunaWeerakoon <it24610788@my.sliit.lk>
2025-10-10 21:47:05 +0530

    Update ProductRepository.java

--- src/main/java/com/example/jewellery_backend/repository/ProductRepository.java
+++ src/main/java/com/example/jewellery_backend/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.example.jewellery_backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.jewellery_backend.entity.Product;
 
+public class ProductRepository {
+}
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // add custom queries here if needed (e.g., findByName)
 }
